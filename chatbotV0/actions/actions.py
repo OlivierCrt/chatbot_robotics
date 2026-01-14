0ee049c848f32f5a04c5ed8c04c0e58621bb6f07
IsmaTIBU <01ismaelmon@gmail.com>
2026-01-14 23:17:00 +0100

    action completed

--- chatbotV0/actions/actions.py
+++ chatbotV0/actions/actions.py
@@ -25,6 +25,8 @@ class ActionIlustrer(Action):
         images = {
             "ask_robot_types": "https://www.robotpark.com/image/data/BLOG_EN/51000/All-Types-Of-Robots-By-Robotpark.png",
             "ask_kinematics": "https://www.mdpi.com/electronics/electronics-13-03304/article_deploy/html/images/electronics-13-03304-g001.png",
+            "ask_articulation": "https://www.memoireonline.com/09/09/2723/Elaboration-dune-strategie-de-coordination-de-mouvements-pour-un-manipulateur-mobile-redondant148.png",
+            "ask_dof": "https://miro.medium.com/v2/resize:fit:1280/format:webp/1*bRZ2SkufbL5QnXCnf9kYSA.jpeg"
         }
 
         image_url = images.get(intent)
